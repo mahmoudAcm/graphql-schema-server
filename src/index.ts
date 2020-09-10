538ce7e3cd190506b023dbe9bff8c22fdeeb3d85
Mahmoud <mahmoud.acm.123@gmail.com>
2020-09-10 18:31:11 +0200

    adding example at index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,32 @@ import serverOptions from "./graphql-schema-server-builder";
 
 const Schema = serverOptions();
 
+const userType = `
+   type User {
+       id: ID!
+       name: String!
+       email: String!
+   }    
+`;
+
+const userQuery = `
+   user: User!
+`;
+
+Schema.setTypeDefsTypes([userType]).setTypeDefsQueries([userQuery]);
+
+const userResolver = {
+  user(){
+      return {
+        id: "1-2",
+        name: "Mahmoud",
+        email: "mahmoud@gmail.com"
+      };
+  }
+};
+
+Schema.setResolversQueries([userResolver]);
+
 const schema = Schema.build();
 
 console.log(schema);
